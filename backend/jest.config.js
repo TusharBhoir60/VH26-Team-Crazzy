b468d80fffc449a9d5ad00baeb1189398c79b30f
Sarasbari <sarasbari07@gmail.com>
2026-09-04 23:03:00 +0530

    feat(lifecycle): implement lifecycle tracking stage

--- backend/jest.config.js
+++ backend/jest.config.js
@@ -1,15 +1,6 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  roots: ['<rootDir>/src'],
-  testMatch: ['**/__tests__/**/*.test.ts'],
-  transform: {
-    '^.+\\.tsx?$': [
-      'ts-jest',
-      {
-        tsconfig: 'tsconfig.json',
-      },
-    ],
-  },
-  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  testMatch: ['**/*.test.ts']
 };
